8b8c6740b7d0e9bb891133caf590f1df9b3e3f43
miz14 <vpashenko14@gmail.com>
2025-08-10 11:53:34 +0600

    fix generateChartData

--- projects/charts/src/lib/chart-echarts/chart-echarts.stories.ts
+++ projects/charts/src/lib/chart-echarts/chart-echarts.stories.ts
@@ -1,5 +1,6 @@
 import { Meta, StoryObj } from '@storybook/angular';
 import { ChartEchartsComponent } from '../../public-api';
+import { generateChartData } from '../data/generate-chart-data';
 
 const meta: Meta<ChartEchartsComponent> = {
     title: 'Echarts/Chart',
@@ -19,36 +20,6 @@ const meta: Meta<ChartEchartsComponent> = {
 };
 export default meta;
 type Story = StoryObj<ChartEchartsComponent>;
-
-const generateChartData = (
-    line_count: number,
-    start_date: Date,
-    end_date: Date,
-    point_count: number
-) => {
-    let current_value = Math.random() * 100;
-
-    const update_value = () => {
-        current_value += (Math.random() - 0.5) * Math.random() * 50;
-        if (current_value > 100) {
-            current_value = 100;
-        } else if (current_value < 0) {
-            current_value = 0;
-        }
-        return current_value;
-    };
-
-    const date_step = Math.ceil(
-        (end_date.getTime() - start_date.getTime()) / point_count
-    );
-    return Array.from({ length: line_count }, (_, i) => ({
-        name: `Data-${i + 1}`,
-        data: Array.from({ length: point_count }, (_, j) => ({
-            time: new Date(start_date.getTime() + j * date_step),
-            value: update_value(),
-        })),
-    }));
-};
 
 export const Default: Story = {
     args: {
